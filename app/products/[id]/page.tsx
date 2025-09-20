e610e7deed8291de0db54a7ea8d6d7073d4a5947
Victor Lobato <pixodevbusiness@gmail.com>
2025-09-20 12:17:33 -0300

    fix

--- app/products/[id]/page.tsx
+++ app/products/[id]/page.tsx
@@ -1,16 +1,18 @@
-import { ProductDetail } from "@/components/ui/product-detail"
-import { stripe } from "@/lib/stripe"
+import { ProductDetail } from "@/components/ui/product-detail";
+import { stripe } from "@/lib/stripe";
 
 export default async function ProductPage({
-    params
-    }:{
-        params: {
-            id: string
-        }}) {
-    const product = await stripe.products.retrieve(params.id, {
-        expand: ["default_price"]
-    });
+  params,
+}: {
+  params: Promise<{
+    id: string;
+  }>;
+}) {
+  const { id } = await params;
+  const product = await stripe.products.retrieve(id, {
+    expand: ["default_price"],
+  });
 
-    const plainProduct= JSON.parse(JSON.stringify(product))
-    return <ProductDetail product={plainProduct} />
+  const plainProduct = JSON.parse(JSON.stringify(product));
+  return <ProductDetail product={plainProduct} />;
 }
